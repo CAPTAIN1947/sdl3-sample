096b543e1f64dd41ad5d37b888781af8c5fdc8f3
Captain <87000456+captain1947@users.noreply.github.com>
2023-08-21 18:54:19 +0530

    Add files via upload

---
+++ Android.mk
@@ -0,0 +1,20 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := main
+
+SDL_PATH := ../SDL
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include \
+					$(SDL_PATH)/include/SDL3
+
+
+# Add your application source files here...
+LOCAL_SRC_FILES := main.cpp
+
+LOCAL_SHARED_LIBRARIES := SDL2
+
+LOCAL_LDLIBS := -lGLESv1_CM -lGLESv2 -lOpenSLES -llog -landroid
+
+include $(BUILD_SHARED_LIBRARY)
